052d8f64603ff36225092a944b56e1bdd52d01d0
九思 <10837907@qq.com>
2024-05-04 18:35:48 +0800

    完成题库基础功能

---
+++ src/main/java/cc/jiusi/springbootinit/mapper/QuestionBankMapper.java
@@ -0,0 +1,74 @@
+package cc.jiusi.springbootinit.mapper;
+
+import cc.jiusi.springbootinit.model.entity.QuestionBank;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @blog: <a href="https://www.jiusi.cc">九思_Java之路</a>
+ * @Author: 九思.
+ * @CreateTime: 2024-05-04 17:16:02
+ * @Description: 题库信息(QuestionBank)表数据库访问层
+ */
+public interface QuestionBankMapper {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    QuestionBank selectById(Long id);
+
+    /**
+     * 查询所有数据
+     *
+     * @param questionBank 查询条件
+     * @return List<QuestionBank> 实例对象列表
+     */
+    List<QuestionBank> selectAll(QuestionBank questionBank);
+
+    /**
+     * 统计总行数
+     *
+     * @param questionBank 查询条件
+     * @return 总行数
+     */
+    long count(QuestionBank questionBank);
+
+    /**
+     * 新增数据
+     *
+     * @param questionBank 实例对象
+     * @return 影响行数
+     */
+    int insert(QuestionBank questionBank);
+
+    /**
+     * 批量新增数据（MyBatis原生foreach方法）
+     *
+     * @param entities List<QuestionBank> 实例对象列表
+     * @return 影响行数
+     */
+    int insertBatch(@Param("entities") List<QuestionBank> entities);
+
+    /**
+     * 修改数据
+     *
+     * @param questionBank 实例对象
+     * @return 影响行数
+     */
+    int update(QuestionBank questionBank);
+
+    /**
+     * 通过主键集合批量删除数据
+     *
+     * @param ids List<Long> 主键id集合
+     * @return 影响行数
+     */
+    int deleteBatchByIds(@Param("ids") List<Long> ids);
+
+    void updateStatus(@Param("ids") List<Long> ids, @Param("status") String status);
+}
+
---
+++ src/main/java/cc/jiusi/springbootinit/model/dto/questionBank/QuestionBankAddRequest.java
@@ -0,0 +1,52 @@
+package cc.jiusi.springbootinit.model.dto.questionBank;
+
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @blog: <a href="https://www.jiusi.cc">九思_Java之路</a>
+ * @Author: 九思.
+ * @CreateTime: 2024-05-02 21:27:53
+ * @Description: 笔记信息(Note)实体类
+ */
+@Data
+@ApiModel(value = "NoteAddRequest", description = "笔记新增请求")
+public class QuestionBankAddRequest implements Serializable {
+    private static final long serialVersionUID = -60799118686942121L;
+    /**
+     * 题库名称
+     */
+    @ApiModelProperty(value = "题库名称")
+    private String name;
+    /**
+     * 题库简介
+     */
+    @ApiModelProperty(value = "题库简介")
+    private String summary;
+    /**
+     * 题库封面
+     */
+    @ApiModelProperty(value = "题库封面")
+    private String coverImg;
+    /**
+     * 题库难度 0-简单 1-一般 2-困难
+     */
+    @ApiModelProperty(value = "题库难度 0-简单 1-一般 2-困难")
+    private String difficulty;
+    /**
+     * 题库状态 0-停用 1-启用
+     */
+    @ApiModelProperty(value = "题库状态 0-停用 1-启用")
+    private String status;
+    /**
+     * 分类id
+     */
+    @ApiModelProperty(value = "分类id")
+    private Long categoryId;
+}
+
